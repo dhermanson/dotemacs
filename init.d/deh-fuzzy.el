ae48c811e4bc74e0a0630a3140d640d07df4cb33
Derick Hermanson <derick.hermanson@gmail.com>
2019-10-03 04:46:32 -0500

    Use urxvtc instead of urxvt when opening fzf-file-finder

--- init.d/deh-fuzzy.el
+++ init.d/deh-fuzzy.el
@@ -4,7 +4,7 @@
       (let ((default-directory (projectile-project-root)))
         (start-process-shell-command "fzf-file-finder" nil
                                      (concat "EMACS_SERVER_NAME=" server-name
-                                             " urxvt -geometry 125x40 -name urxvtfloat -e 'deh-find-file' 2> /dev/null")))))
+                                             " urxvtc -geometry 125x40 -name urxvtfloat -e 'deh-find-file' 2> /dev/null")))))
 
 ;; TODO: don't do this in gui environments...instead, do above
 ;; (defun deh-projectile-fzf-find-file ()
