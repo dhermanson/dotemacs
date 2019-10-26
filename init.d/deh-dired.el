302b34dec19e876af54ca236af1f2b4828cbeef8
Derick Hermanson <derick.hermanson@gmail.com>
2019-10-26 07:06:19 -0500

    Don't hide details in dired

--- init.d/deh-dired.el
+++ init.d/deh-dired.el
@@ -6,7 +6,8 @@
   "My dired mode hook."
 
   (put 'dired-find-alternate-file 'disabled nil)
-  (dired-hide-details-mode 1))
+  (dired-hide-details-mode nil)
+  )
 
 (add-hook 'dired-mode-hook 'deh/dired-mode-hook)
 
