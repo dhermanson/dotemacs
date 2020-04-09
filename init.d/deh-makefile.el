592f86560d388cf203ce5e596c245399e67bee99
Derick Hermanson <dhermanson@bushelpowered.com>
2020-04-09 06:41:33 -0500

    Associate [Mm]akefile files with makefile-mode

--- init.d/deh-makefile.el
+++ init.d/deh-makefile.el
@@ -9,6 +9,9 @@
   (setq indent-tabs-mode t)
   (set (make-local-variable 'company-backends) '((company-yasnippet company-dabbrev-code))))
 
+
 (add-hook 'makefile-mode-hook 'deh-makefile-mode-hook)
+
+(add-to-list 'auto-mode-alist '("[Mm]akefile" . makefile-mode))
 
 (provide 'deh-makefile)
