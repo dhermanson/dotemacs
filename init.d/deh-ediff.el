7697e3c12016522f67478d208655e461453fc195
Derick Hermanson <derick.hermanson@gmail.com>
2020-12-14 21:42:02 -0600

    switch back to 'split-window-horizontally for ediff

--- init.d/deh-ediff.el
+++ init.d/deh-ediff.el
@@ -1,8 +1,8 @@
 ;; (setq-default ediff-forward-word-function 'forward-char)
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 
-;; (setq ediff-split-window-function 'split-window-horizontally)
-(setq ediff-split-window-function 'split-window-vertically)
+(setq ediff-split-window-function 'split-window-horizontally)
+;; (setq ediff-split-window-function 'split-window-vertically)
 (setq ediff-highlight-all-diffs t)
 (setq ediff-diff-options "-w") ;; ignore whitespace
 
