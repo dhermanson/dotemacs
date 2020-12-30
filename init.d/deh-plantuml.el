441d16e50f6784ccc9582501a15f9d0b8b3bc8f2
Derick Hermanson <derick.hermanson@gmail.com>
2020-12-30 12:41:52 -0600

    updates to plantuml mode

--- init.d/deh-plantuml.el
+++ init.d/deh-plantuml.el
@@ -10,14 +10,22 @@
   "my plantuml hook"
   (company-mode)
   (flycheck-mode)
+
+  (setq display-line-numbers 'relative)
   (smartparens-mode)
   (set (make-local-variable 'company-backends) '((company-dabbrev-code company-yasnippet)))
   (setq evil-shift-width 2))
 
-(setq plantuml-jar-path "~/.config/deh/jars/plantuml.jar")
+;; (setq plantuml-jar-path "~/.config/deh/jars/plantuml.jar")
+(setq-default plantuml-exec-mode 'jar)
 (add-hook 'plantuml-mode-hook 'deh-plantuml-hook)
 
 (add-to-list 'auto-mode-alist '("\\.puml$" . plantuml-mode))
 (add-to-list 'auto-mode-alist '("\\.plantuml$" . plantuml-mode))
+
+(with-eval-after-load 'flycheck
+  (require 'flycheck-plantuml)
+  (flycheck-plantuml-setup))
+
 
 (provide 'deh-plantuml)
