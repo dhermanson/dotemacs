d2299b0cb49a288a4d501a22a601875f1a41b7b4
Derick Hermanson <derick.hermanson@gmail.com>
2020-12-30 23:58:45 -0600

    update deh/hydras/windows

--- init.d/deh-hydra.el
+++ init.d/deh-hydra.el
@@ -1,11 +1,35 @@
 (require 'hydra)
 
 
-(defhydra deh/hydras/windows (nil nil)
-  "other window"
+(defhydra deh/hydras/windows (global-map "C-c w")
+  "windows"
   ("o" #'other-window "next window")
-  ("O" (lambda () (interactive) (other-window -1)) "previous window"))
+  ("O" (lambda () (interactive) (other-window -1)) "previous window")
+  ;; ("o" #'delete-other-windows "delete other windows")
+
+  ;; create windows
+  ("s" #'split-window-below "split window below")
+  ("v" #'split-window-right "split window right")
+
+  ;; evil windows
+  ("h" #'deh-windmove-create-or-select-window-left "window left")
+  ("j" #'deh-windmove-create-or-select-window-down "window down")
+  ("k" #'deh-windmove-create-or-select-window-up "window up")
+  ("l" #'deh-windmove-create-or-select-window-right "window right")
+  ("H" #'evil-window-move-far-left "move window to far left")
+  ("J" #'evil-window-move-very-bottom "move window to very bottom")
+  ("K" #'evil-window-move-very-top "move window to very top")
+  ("L" #'evil-window-move-far-right "move window to far right")
+  ("c" #'evil-window-delete "delete window")
+
+  ;; quit
+  ("C-g" nil "quit")
+  ("g" nil "quit")
+  ("q" nil "quit"))
+
+
 
 (define-key global-map (kbd "C-x o") 'deh/hydras/windows/other-window)
+
 
 (provide 'deh-hydra)
